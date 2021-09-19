a9242df3dbb7a06c8a1fc3f25460c8a76d70d16c
Montes Morales, David Ernesto <DAVIDMON@IADB.ORG>
2021-09-19 18:28:41 -0400

    change || config for aws

--- src/main/java/com/insuranceagency/awsinsurance/demo/HelloWorldController.java
+++ src/main/java/com/insuranceagency/awsinsurance/demo/HelloWorldController.java
@@ -9,7 +9,7 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
-        return "Hello World";
+        return "Hello World - ints";
     }
 
     @GetMapping(path = "/hello-world-bean")
