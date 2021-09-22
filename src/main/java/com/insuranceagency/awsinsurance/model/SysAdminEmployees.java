424f0e6f9696a1ce3d4b8038c004194a1bbd58e2
notkooru <notkooru@gmail.com>
2021-09-21 19:32:35 -0600

    Created SysAdminEmployees class

---
+++ src/main/java/com/insuranceagency/awsinsurance/model/SysAdminEmployees.java
@@ -0,0 +1,27 @@
+package com.insuranceagency.awsinsurance.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "SysAdmin")
+@ToString @EqualsAndHashCode
+
+public class SysAdminEmployees {
+
+    @Getter @Setter @Column(name = "idSysAdminEmployee")
+    private Long idSysAdminEmployee;
+
+    @Getter @Setter @Column(name = "idSysAdmin")
+    private Long idSysAdmin;
+
+    @Getter @Setter @Column(name = "idEmployees")
+    private Long idEmployees;
+
+    @Getter @Setter @Column(name = "active")
+    private boolean active;
+}
