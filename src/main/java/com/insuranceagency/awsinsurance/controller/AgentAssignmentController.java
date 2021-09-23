968882cd2007fd44e8e26f8546025764616afb3a
Stanley <wltrstnly@hotmail.com>
2021-09-22 21:39:27 -0600

    Changes

--- src/main/java/com/insuranceagency/awsinsurance/controller/AgentAssignmentController.java
+++ src/main/java/com/insuranceagency/awsinsurance/controller/AgentAssignmentController.java
@@ -1,15 +1,14 @@
 package com.insuranceagency.awsinsurance.controller;
 
 import com.insuranceagency.awsinsurance.dao.AgentAssignmentRepository;
-import com.insuranceagency.awsinsurance.dao.impl.AgentAssignmentServiceImpl;
 import com.insuranceagency.awsinsurance.model.AgentAssignment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class AgentAssignmentController {
@@ -25,10 +24,36 @@ public class AgentAssignmentController {
         return test;
     }
 
-    @RequestMapping(value = "api/AgentAssignments", method = RequestMethod.POST)
+    @RequestMapping(value = "api/agentAssignments", method = RequestMethod.POST)
     public AgentAssignment saveAgentAssignment(@RequestBody AgentAssignment agentAssignment){
         AgentAssignment response = agentAssignmentRepository.save(agentAssignment);
         return response;
+    }
+
+    @RequestMapping(value = "api/agentAssignment/{id}", method = RequestMethod.PUT)
+    public AgentAssignment updateAgentAssignment(@PathVariable("id") long idAgentAssignments, @RequestBody AgentAssignment agentAssignment){
+        Optional<AgentAssignment> data = agentAssignmentRepository.findById(idAgentAssignments);
+        if(data.isPresent()){
+            AgentAssignment _agentAssignment = data.get();
+            _agentAssignment.setIdAgentAssignments(agentAssignment.getIdAgentAssignments());
+            _agentAssignment.setIdEmployees(agentAssignment.getIdEmployees());
+            _agentAssignment.setIdAgent(agentAssignment.getIdAgent());
+            _agentAssignment.setActive(agentAssignment.isActive());
+            AgentAssignment response = agentAssignmentRepository.save(_agentAssignment);
+            return response;
+        }else{
+            return null;
+        }
+    }
+
+    @RequestMapping(value = "api/agentAssignment/delete/{id}")
+    public ResponseEntity<HttpStatus> deleteAgentAssignment(@PathVariable("id") long idAgentAssignments){
+        try{
+            agentAssignmentRepository.deleteById(idAgentAssignments);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
