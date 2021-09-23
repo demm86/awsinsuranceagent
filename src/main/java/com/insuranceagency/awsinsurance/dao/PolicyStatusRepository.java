003d9799ae267e42efeee5d33cf014782e0b9306
@Martinez182 <emre21011992@gmail.com>
2021-09-22 22:51:39 -0600

    Creating policystatus repo

--- src/main/java/com/insuranceagency/awsinsurance/dao/PolicyStatusRepository.java
+++ src/main/java/com/insuranceagency/awsinsurance/dao/PolicyStatusRepository.java
@@ -3,9 +3,11 @@ package com.insuranceagency.awsinsurance.dao;
 import com.insuranceagency.awsinsurance.model.Client;
 import com.insuranceagency.awsinsurance.model.PolicyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PolicyStatusRepository extends JpaRepository<PolicyStatus, Long> {
 
 
