d140cec7f40dda1038afbd214460d3222a02c310
notkooru <notkooru@gmail.com>
2021-09-23 03:11:08 -0600

    Implemented RolRepository with JpaRepository

--- src/main/java/com/insuranceagency/awsinsurance/dao/RolRepository.java
+++ src/main/java/com/insuranceagency/awsinsurance/dao/RolRepository.java
@@ -1,7 +1,7 @@
 package com.insuranceagency.awsinsurance.dao;
 
-public interface RolRepository {
+import com.insuranceagency.awsinsurance.model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
+public interface RolRepository extends JpaRepository<Rol, Long>{
 }
