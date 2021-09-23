88d805cc0ca3062a96720a064cd6be9b9da089c4
@Martinez182 <emre21011992@gmail.com>
2021-09-23 15:47:15 -0600

    policy status repo, serv, imp, cont

--- src/main/java/com/insuranceagency/awsinsurance/controller/PolicyStatusConotroller.java
+++ src/main/java/com/insuranceagency/awsinsurance/controller/PolicyStatusConotroller.java
@@ -1,13 +1,14 @@
 package com.insuranceagency.awsinsurance.controller;
 
-import com.insuranceagency.awsinsurance.dao.PolicyStatusRepository;
-import com.insuranceagency.awsinsurance.model.AgentAssignment;
+import com.insuranceagency.awsinsurance.dao.PolicyStatusService;
+import com.insuranceagency.awsinsurance.model.Employee;
+import com.insuranceagency.awsinsurance.repository.PolicyStatusRepository;
 import com.insuranceagency.awsinsurance.model.PolicyStatus;
+import com.insuranceagency.awsinsurance.utils.RegisterExistException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,19 +18,42 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class PolicyStatusConotroller {
 
     @Autowired
-    private PolicyStatusRepository policyStatusRepository;
+    private PolicyStatusService policyStatusService;
 
     @RequestMapping(value = "api/policystatus", method = GET)
 
     public List<PolicyStatus> getPolicyStatus() {
-        List<PolicyStatus> test = policyStatusRepository.findAll();
+        List<PolicyStatus> test = policyStatusService.getPolicyStatus();
         test.forEach(System.out::println);
         return test;
     }
 
-    @RequestMapping(value = "api/policystatus", method = RequestMethod.POST)
-    public PolicyStatus savePolicyStatus(@RequestBody PolicyStatus policyStatus){
-        PolicyStatus response = policyStatusRepository.save(policyStatus);
-        return response;
+    @RequestMapping(value = "/api/policystatus", method = RequestMethod.POST)
+    public ResponseEntity<PolicyStatus> savePolicyStatus(@RequestBody PolicyStatus policyStatus) {
+        try {
+            PolicyStatus response = policyStatusService.savePolicyStatus(policyStatus);
+            return new ResponseEntity<PolicyStatus>(response, HttpStatus.OK);
+
+        } catch (RegisterExistException ex) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @RequestMapping(value = "/api/policystatus", method = RequestMethod.PUT)
+    public PolicyStatus updatePolicyStatus(@RequestBody PolicyStatus policyStatus) {
+        return policyStatusService.updatePolicyStatus(policyStatus);
+    }
+
+
+    @RequestMapping(value = "api/policystatus/{id}", method = RequestMethod.DELETE)
+    public void deletePolicyStatus(@PathVariable Long id) {
+        policyStatusService.deletePolicyStatusById(id);
+    }
+
+    @RequestMapping(value = "/api/description/{desc}", method = RequestMethod.GET)
+    public PolicyStatus policyStatusDescription (@PathVariable String desc) {
+        return policyStatusService.getPolicyStatusByDescription(desc);
     }
 }
---
+++ src/main/java/com/insuranceagency/awsinsurance/dao/PolicyStatusService.java
@@ -0,0 +1,16 @@
+package com.insuranceagency.awsinsurance.dao;
+
+import com.insuranceagency.awsinsurance.model.Employee;
+import com.insuranceagency.awsinsurance.model.PolicyStatus;
+
+import java.util.List;
+
+public interface PolicyStatusService {
+    List<PolicyStatus> getPolicyStatus();
+
+    PolicyStatus savePolicyStatus(PolicyStatus policyStatus) throws Exception;;
+    PolicyStatus updatePolicyStatus(PolicyStatus policyStatus);
+    PolicyStatus getPolicyStatusById(Long id);
+    PolicyStatus getPolicyStatusByDescription(String descriptionInfo);
+    void deletePolicyStatusById(Long id);
+}
