e1c03102cd404efb56aea03731f4e1c1d7fb74ae
Montes Morales, David Ernesto <DAVIDMON@IADB.ORG>
2021-09-24 14:06:20 -0400

    change || web config cors

--- src/main/java/com/insuranceagency/awsinsurance/utils/WebSecurityConfig.java
+++ src/main/java/com/insuranceagency/awsinsurance/utils/WebSecurityConfig.java
@@ -63,5 +63,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        httpSecurity.cors();
+        httpSecurity.cors().and();
+
     }
 }
