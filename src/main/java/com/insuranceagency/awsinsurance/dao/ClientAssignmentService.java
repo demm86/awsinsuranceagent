8447003a5dc6f59afd69da45cc262a24754578a6
Stanley <wltrstnly@hotmail.com>
2021-09-25 17:04:13 -0600

    commit

--- src/main/java/com/insuranceagency/awsinsurance/dao/ClientAssignmentService.java
+++ src/main/java/com/insuranceagency/awsinsurance/dao/ClientAssignmentService.java
@@ -1,4 +1,15 @@
 package com.insuranceagency.awsinsurance.dao;
 
+import com.insuranceagency.awsinsurance.model.ClientAssignment;
+
+import java.util.List;
+
 public interface ClientAssignmentService {
+
+    List<ClientAssignment> getClientAssignment();
+
+    ClientAssignment saveClientAssignment(ClientAssignment clientAssignment);
+    ClientAssignment updateClientAssignment(ClientAssignment clientAssignment);
+    ClientAssignment getClientAssignmentById(Long id);
+    void deleteClientAssignmentById(Long id);
 }
